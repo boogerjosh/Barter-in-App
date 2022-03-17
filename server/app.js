cbff773c53968702a6a3bd94c6f166c33465416f
glenn2206 <leonardusglenn@gmail.com>
2022-03-18 00:22:41 +0700

    minor revision socketio

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const router = require("./routes/index");
