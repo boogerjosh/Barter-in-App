a34a1fa0b0f002f1e73f151d7ccedc319a2cf1c3
“haryosmd” <haryosmd@gmail.com>
2022-03-19 23:02:39 +0700

    chose category done

--- client/barter-in-mobile-app/src/screens/ChooseCategory.js
+++ client/barter-in-mobile-app/src/screens/ChooseCategory.js
@@ -7,6 +7,7 @@ import {
   FlatList,
   TouchableOpacity,
   Dimensions,
+  SafeAreaView,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
@@ -14,22 +15,39 @@ import FONTS from "../constants/Fonts";
 import COLORS from "../constants/Colors";
 const { height, width } = Dimensions.get("screen");
 const setWidth = (w) => (width / 100) * w;
-
+import categoryAdd from "../../data/categoryAdd";
+import CategoryInputCard from "../components/CategoryInputCard";
+import ItemSpace from "../components/ItemSpace";
+const numColumns = 2;
 const ChooseCategory = () => {
   const navigation = useNavigation();
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text>Ini Profile Screen</Text>
-
-        <TouchableOpacity
-          style={styles.button}
-          activeOpacity={0.8}
-          onPress={() => navigation.push("Home", {})}
-        >
-          <Text style={styles.buttonText}>Home</Text>
-        </TouchableOpacity>
+      <SafeAreaView style={styles.header}>
+        <View style={styles.headerWrapper}>
+          <View style={styles.headerDetails}>
+            <View>
+              <Text style={styles.nameText}>Choose Item category</Text>
+            </View>
+          </View>
+          <View>
+            {/* <Image
+              source={require("../../assets/person.jpg")}
+              style={styles.headerImage}
+            /> */}
+          </View>
+        </View>
+      </SafeAreaView>
+      <View>
+        <FlatList
+          data={categoryAdd}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => <ItemSpace width={10} />}
+          ListHeaderComponent={() => <ItemSpace width={10} />}
+          ListFooterComponent={() => <ItemSpace width={10} />}
+          renderItem={({ item }) => <CategoryInputCard item={item} />}
+          numColumns={numColumns}
+        />
       </View>
     </ScrollView>
   );
@@ -37,10 +55,70 @@ const ChooseCategory = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  header: {
+    backgroundColor: COLORS.BASIC_BACKGROUND,
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
     alignItems: "center",
     justifyContent: "center",
+  },
+  headerWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 20,
+    paddingBottom: 30,
+  },
+  headerImage: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    // borderColor: COLORS.WHITE,
+    borderWidth: 2,
+  },
+  headerDetails: {
+    // flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  iconWrapper: {
+    marginLeft: 10,
+  },
+  nameText: {
+    fontSize: 20,
+    color: COLORS.EXTRA_LIGHT_GRAY,
+    fontFamily: FONTS.BOLD,
+  },
+  search: {
+    marginHorizontal: 20,
+    backgroundColor: COLORS.WHITE,
+    borderRadius: 20,
+    padding: 15,
+    marginTop: -25,
+    shadowColor: COLORS.BLACK,
+    shadowOffset: {
+      height: 3,
+      width: 0,
+    },
+    shadowRadius: 4,
+    shadowOpacity: 0.1,
+    elevation: 1,
+  },
+  searchWrapper: {
+    flexDirection: "row",
+  },
+  searchicon: {
+    marginRight: 10,
+    color: COLORS.DARK_GREY,
+  },
+  searchInput: {
+    color: COLORS.DARK_GREY,
+    fontFamily: FONTS.MEDIUM,
   },
   button: {
     justifyContent: "center",
