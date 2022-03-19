24baa39e764a4e9e9ea203d6e6d71d58c4885b43
glenn2206 <leonardusglenn@gmail.com>
2022-03-19 22:48:35 +0700

    socket.io testing

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(errorHandler);
 //   console.log(`Example app listening on port ${port}`)
 // })
 
-// module.exports = app;
+module.exports = app;
 
 // const messageArray = [];
 // io.on("connection", (socket) => {
